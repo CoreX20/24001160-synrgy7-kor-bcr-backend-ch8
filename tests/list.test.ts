0e5db475ca6515126fa5833bb60b0cb2b6075de7
CoreX20 <korneliussetiawan13@gmail.com>
2024-06-28 15:25:44 +0700

    update main.yml

--- tests/list.test.ts
+++ tests/list.test.ts
@@ -5,7 +5,7 @@ describe('GET /cars', () => {
     it('should response with 200 as status code', async () => {
       await request(app)
         .get('/cars')
-        .set('Accept', 'application/json')
+        // .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
     });
